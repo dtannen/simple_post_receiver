d349701493fed06c7a18a5d9d5a9ac687bf42735
David Tannenbaum <dtannen@gmail.com>
2015-06-18 15:20:14 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export GOPATH=`pwd`
+echo "[-] Get Packages"
+echo "[-] Go Build"
+go build -o cal_receiver
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func ScheduleOnce(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ScheduleOnce message received")
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if r.Method == "POST" {
+		fmt.Println("Post message received")
+		for k, v := range r.Form {
+			fmt.Println("key :", k)
+			fmt.Println("value :", strings.Join(v, ""))
+		}
+		f, err := os.OpenFile("sologfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("error opening file: %v", err)
+			return
+		}
+		defer f.Close()
+
+		log.SetOutput(f)
+		body, err := ioutil.ReadAll(r.Body)
+		log.Println(string(body))
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("HTTP status code returned!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/scheduleonce", ScheduleOnce)
+	http.ListenAndServe(":8081", mux)
+}
